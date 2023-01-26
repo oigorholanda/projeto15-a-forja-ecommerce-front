15796a7529ef4406675d402f379b6866f10ad594
Igor Holanda <104221105+oigorholanda@users.noreply.github.com>
2023-01-26 10:49:36 -0300

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,9 @@
 import styled from "styled-components"
-import Footer from "./Footer"
-import Header from "./Header"
 
 export default function Cart() {
     return (
         <ConteinerCart>
-        
         Seu carrinho
-        
         </ConteinerCart>
     )
 }
