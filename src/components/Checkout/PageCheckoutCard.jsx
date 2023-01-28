a269bdf56824c93602da791dc906344a8934df57
teuprimo <vinicius.brandaot@gmail.com>
2023-01-28 18:23:47 -0300

    purchase finalization implemented

--- src/components/Checkout/PageCheckoutCard.jsx
+++ src/components/Checkout/PageCheckoutCard.jsx
@@ -2,8 +2,10 @@ import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { Container, Title, Description, LowerContainer, SubmitBtn, CheckoutConfirm} from "./CheckoutStyles";
 import axios from "axios";
+import { useState } from "react";
 
 export default function App() {
+    const [finalized, setFinalized] = useState(false);
     const url =  "http://localhost:5000"
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const Authorization = "5a284611-1be4-42f9-a3f3-0e196394b29e"
@@ -21,14 +23,14 @@ export default function App() {
             }
           }
         const response = axios.post(`${url}/paymentinfo`, data, config)
-        response.then(() => "")
+        response.then(() => setFinalized(true))
         response.catch((error) => {
             console.log(error.response)
             return error.response;
         });
     };
   
-    return(
+    if(!finalized){return(
         <>
         
         <Container>
@@ -61,11 +63,50 @@ export default function App() {
     <SubmitBtn> <input value="Cadastrar" style={{background:"#ffa375", color:'white'}} type="submit" /></SubmitBtn>
     </form>
         </div>
-        {/* <CheckoutConfirm >
-            <p> Você acaba de concluir sua compra no valor de (R$ ).</p>
-            <p> Em breve seu pedido será enviado. </p> */}
-            {/* <button style={{background:'#cecece'}} onClick={() => setDisplay('none')}>NAO</button> <button onClick={() => onSubmit(0)} style={{background:'#ff4791'}}>SIM</button> */}
-         {/* </CheckoutConfirm> */}
         </Container>
         </>
     )}
+    else{return(
+        <>
+        
+        <Container>
+        
+        <Title>
+        
+        <span><h1>A Forja</h1> <br/></span>
+        </Title>
+        <Description>
+       
+        <div><h3> Preco:</h3> <h3>&emsp;&nbsp;R$</h3> <br/></div>
+        
+        </Description>
+        <div>
+        <form onSubmit={handleSubmit(onSubmit)} {...register("membershipId")}>
+        
+        <div><input placeholder="Nome impresso no cartao" id='name' name="name" type="name" {...register("cardName", {required:true})} /></div>
+        {errors.email && <div><h3>Insira seu Email</h3></div>}
+
+        <div><input placeholder="Digitos do cartao" id='number' name="number" type="number" {...register("cardNumber", {required:true})} /></div>
+        {errors.email && <div><h3>Digite Seu CPF</h3></div>}
+
+        <LowerContainer>
+        <div><input placeholder="codigo de seguranca" id='number' name="number" type="number" {...register("securityNumber", {required:true , valueAsNumber:true})} /></div>
+        {errors.email && <div><h3>Insira seu Email</h3></div>}
+        
+        <div><input id='pwd' name="pwd" placeholder="validade" type="month" {...register("expirationDate", { required: true })} /></div>
+        {errors.password && <div>Digite sua senha correta!</div>}
+        </LowerContainer>
+    <SubmitBtn> <input value="Cadastrar" style={{background:"#ffa375", color:'white'}} type="submit" /></SubmitBtn>
+    </form>
+        </div>
+        <CheckoutConfirm >
+            <p> Você acaba de concluir sua compra no valor de (R$ ).</p>
+            <p> Em breve seu pedido será enviado. </p>
+            <button style={{background:'#ffa375'}} onClick={() => navigate("/")}>Finalizar.</button>
+         </CheckoutConfirm>
+        </Container>
+        </>
+    )
+
+    }
+}
